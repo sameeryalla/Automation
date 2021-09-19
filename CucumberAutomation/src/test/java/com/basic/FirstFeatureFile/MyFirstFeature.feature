71ff2bf5c91672eb4c7184cce02d3bae59d050a9
sameeryalla <sameeryalla@gmail.com>
2021-09-19 21:13:08 +0530

    adding test runner file to project

--- CucumberAutomation/src/test/java/com/basic/FirstFeatureFile/MyFirstFeature.feature
+++ CucumberAutomation/src/test/java/com/basic/FirstFeatureFile/MyFirstFeature.feature
@@ -3,14 +3,14 @@ As a user I want to login Facebook and check username at home page
 
 
 Scenario: Validate user navigated to login page.
-Given user need to be navigate to FB logoin page
-When user enters username as "username"
+Given user need to be navigate to FB login page
+When user enters username as "dakotafannings"
 And user enters password as "password"
 Then Signin button should be clickable
 
-Scenario: Facebook login with valid credentials.
+Scenario: Facebook login with invalid credentials.
 Given user need to be navigate to FB login page
-When user enters username as "username"
+When user enters username as "dakotafannings"
 And user enters password as "password"
 And click on Login button
-Then user need to check username in homepage 
+Then invalid username and password error message check 
--- CucumberAutomation/src/test/java/com/basic/FirstFeatureFile/MyFirstStepDefinition.java
+++ CucumberAutomation/src/test/java/com/basic/FirstFeatureFile/MyFirstStepDefinition.java
@@ -1,15 +1,66 @@
 package com.basic.FirstFeatureFile;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import junit.framework.Assert;
 
 public class MyFirstStepDefinition {
+	 WebDriver driver;
+	 
 
 	@Given("user need to be navigate to FB login page")
 	public void OpenFBLoginPage()
 	{
-		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\SAMEER YALLA\\Documents\\Automation\\demo\\Automation\\CucumberSetup\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("http://www.facebook.com/#");
+		System.out.println("Login page openned");
 	}
 	
+	@When("user enters username as {string}")
+	public void user_enters_username_as(String username) {
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys(username);
+	    System.out.println("Username entered "+username);
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@When("user enters password as {string}")
+	public void user_enters_password_as(String password) {
+		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys(password);
+		System.out.println("password entered"+password);
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Signin button should be clickable")
+	public void signin_button_should_be_clickable() {
+		if(driver.findElement(By.xpath("//button[@name='login']")).isEnabled())
+		{System.out.println("Sign in button clickable");
+		//Assert.pass();
+		}
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("click on Login button")
+	public void click_on_login_button() {
+		driver.findElement(By.xpath("//button[@name='login']")).click();
+		System.out.println("cliked login button");
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("invalid username and password error message check")
+	public void user_need_to_check_username_in_homepage() {
+		if(driver.findElement(By.xpath("//*[@id='loginform']/div[2]/div[2]/a")).isDisplayed())
+		{System.out.println("navigated to home page");}
+	    throw new io.cucumber.java.PendingException();
+	}
+
 	
 	
 }
