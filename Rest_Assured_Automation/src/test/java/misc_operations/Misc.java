de9d8d8377283cc0c1561bdf506c212f77d06313
sameeryalla <sameeryalla@gmail.com>
2022-11-27 21:27:30 +0530

    validation with matcher changes added

--- Rest_Assured_Automation/src/test/java/misc_operations/Misc.java
+++ Rest_Assured_Automation/src/test/java/misc_operations/Misc.java
@@ -9,11 +9,13 @@ import java.util.concurrent.TimeUnit;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.config.XmlConfig;
+import io.restassured.response.Response;
 
 public class Misc {
 	
@@ -78,5 +80,37 @@ public class Misc {
 		      
 		
 	}
+	
+	@Test
+	public void validate_response_using_response_part()
+	{
+		 Response res=when()
+		      .get("/get-article/bash").
+		 then()
+		      .log().all()
+		      .extract().response();
+		 String href=res.path("href");
+		 String arID=res.path("articleId");
+		 String arURL=res.path("articleUrl");
+		 
+		 //Assert.assertTrue(arID.equals("bash-scripting"));
+		 //Assert.assertTrue(arURL.equalsIgnoreCase("https://www.dezlearn.com/bash-scripting"));
+		 Assert.assertTrue(arURL.equals(href+arID));
+		      
+		
+	}
+	
+	@Test
+	public void validate_response_matcher_example()
+	{
+		 when()
+		      .get("/get-article/bash").
+		 then()
+		      .log().all()
+		      .body("articleUrl", response ->equalTo(response.path("href").toString()+response.path("articleId").toString()));	 
+		      
+		
+	}
+	
 	
 }
